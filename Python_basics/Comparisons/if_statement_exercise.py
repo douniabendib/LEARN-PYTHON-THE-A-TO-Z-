73a2c5a0e44031fee3631146ff41f839076e299e
bendibdounia <bendib.dounia@gmail.com>
2025-05-19 10:54:23 +0100

    Doc: add If Statement exercise

---
+++ Python_basics/Comparisons/if_statement_exercise.py
@@ -0,0 +1,14 @@
+"""
+The variables a and b have missing values. Fill them so that the code inside the if statement will be executed! (make sure the if condition is true)
+
+At the end of the program, the value of c should be 3.
+"""
+a = 12
+b = 12
+c = 0
+
+if a < b or b >= 10:
+    c = 2
+
+c += 1
+print(f"c = {c}")
